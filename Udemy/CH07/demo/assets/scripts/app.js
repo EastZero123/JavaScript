71984329ccb94de20468f27dbdc5fc5faa36d875
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-08-30 07:40:42 +0900

    CH07 demo checkpoint

---
+++ Udemy/CH07/demo/assets/scripts/app.js
@@ -0,0 +1,31 @@
+const addMovieModal = document.getElementById("add-modal");
+// const addMovieModal = document.querySelector("#add-modal");
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector("header button");
+
+const backdrop = document.getElementById("backdrop");
+
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
