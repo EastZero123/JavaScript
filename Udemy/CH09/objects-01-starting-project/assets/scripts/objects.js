57bc38bbf997af32852590d818b79c1d3547e1d8
EastZero123 <cdy1005p@naver.com>
2022-09-05 22:17:38 +0900

    CH09 finish

--- Udemy/CH09/objects-01-starting-project/assets/scripts/objects.js
+++ Udemy/CH09/objects-01-starting-project/assets/scripts/objects.js
@@ -1,34 +1,34 @@
-const addMovieBtn = document.getElementById("add-movie-btn");
-const searchBtn = document.getElementById("search-btn");
+'use strict';
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = (filter = "") => {
-  const movieList = document.getElementById("movie-list");
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
 
-  if (movieList.length === 0) {
-    movieList.classList.remove("visible");
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
     return;
   } else {
-    movieList.classList.add("visible");
+    movieList.classList.add('visible');
   }
-
-  movieList.innerHTML = "";
+  movieList.innerHTML = '';
 
   const filteredMovies = !filter
     ? movies
-    : movies.filter((movie) => {
-        return movie.info.title.includes(filter);
-      });
+    : movies.filter(movie => movie.info.title.includes(filter));
 
-  filteredMovies.forEach((movie) => {
-    const movieEl = document.createElement("li");
+  filteredMovies.forEach(movie => {
+    const movieEl = document.createElement('li');
     const { info, ...otherProps } = movie;
-    // movieEl.textContent = movie.info.title + "-" + movie.info[extraName];
-    const { title: movieTitle } = info;
-    let text = movieTitle + " - ";
+    console.log(otherProps);
+    // const { title: movieTitle } = info;
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.apply(movie) + ' - ';
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== 'title' && key !== '_title') {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -38,34 +38,50 @@ const renderMovies = (filter = "") => {
 };
 
 const addMovieHandler = () => {
-  const title = document.getElementById("title").value;
-  const extraName = document.getElementById("extra-name").value;
-  const extraValue = document.getElementById("extra-value").value;
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
 
   if (
-    title.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
   ) {
     return;
   }
 
   const newMovie = {
     info: {
-      title,
-      [extraName]: extraValue,
+      set title(val) {
+        if (val.trim() === '') {
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
+      [extraName]: extraValue
     },
     id: Math.random().toString(),
+    getFormattedTitle() {
+      console.log(this);
+      return this.info.title.toUpperCase();
+    }
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
 };
 
 const searchMovieHandler = () => {
-  const filterTerm = document.getElementById("filter-title").value;
+  console.log(this);
+  const filterTerm = document.getElementById('filter-title').value;
   renderMovies(filterTerm);
 };
 
-addMovieBtn.addEventListener("click", addMovieHandler);
-searchBtn.addEventListener("click", searchMovieHandler);
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
